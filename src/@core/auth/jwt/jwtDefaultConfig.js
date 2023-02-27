401718dede93887cfa34138bfddffcaf8e75cb95
Satpal <deepsatpal>
2023-02-27 18:51:54 +0530

    changes

--- src/@core/auth/jwt/jwtDefaultConfig.js
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -1,16 +1,16 @@
 export default {
   // Endpoints
-  loginEndpoint: 'http://localhost:8000/auth/token/login/',
-  registerEndpoint: 'http://localhost:8000/auth/users/',
+  loginEndpoint: 'http://singelsajten.se:8000/auth/token/login/',
+  registerEndpoint: 'http://singelsajten.se:8000/auth/users/',
   refreshEndpoint: '/jwt/refresh-token',
   logoutEndpoint: '/jwt/logout',
-  getCustomerEndpoint: 'http://localhost:8000/api/moderator/customers/',
-  profileEndpoint: 'http://localhost:8000/auth/users/me/',
-  createModelEndpoint: 'http://127.0.0.1:8000/api/moderator/model/',
-  getLikedGirlsEndpoint: 'http://127.0.0.1:8000/api/moderator/liked-girls/',
-  userLikeEndpoint: 'http://127.0.0.1:8000/api/moderator/user-like/',
-  randomGirlEndpoint: 'http://127.0.0.1:8000/api/moderator/random-girl/',
-  createChatEndpoint: 'http://127.0.0.1:8000/api/chat/',
+  getCustomerEndpoint: 'http://singelsajten.se:8000/api/moderator/customers/',
+  profileEndpoint: 'http://singelsajten.se:8000/auth/users/me/',
+  createModelEndpoint: 'http://singelsajten.se:8000/api/moderator/model/',
+  getLikedGirlsEndpoint: 'http://singelsajten.se:8000/api/moderator/liked-girls/',
+  userLikeEndpoint: 'http://singelsajten.se:8000/api/moderator/user-like/',
+  randomGirlEndpoint: 'http://singelsajten.se:8000/api/moderator/random-girl/',
+  createChatEndpoint: 'http://singelsajten.se:8000/api/chat/',
   // This will be prefixed in authorization header with token
   // e.g. Authorization: Bearer <token>
   tokenType: 'Token',
--- src/views/Moderator/chat/test.js
+++ src/views/Moderator/chat/test.js
@@ -1,23 +1,23 @@
 export default {
   // Endpoints
-  socketHost: '12.0.0.1:8080',
-  loginEndpoint: 'http://localhost:8000/auth/token/login/',
-  registerEndpoint: 'http://localhost:8000/auth/users/',
+  socketHost: 'singelsajten.se',
+  loginEndpoint: 'http://singelsajten.se:8000/auth/token/login/',
+  registerEndpoint: 'http://singelsajten.se:8000/auth/users/',
   createModelEndpoint: '/api/moderator/model/',
   createModeratorEndpoint: '/api/admin/moderator/',
   getCustomerEndpoint: '/api/moderator/customers/',
   uploadProfilePhotoEndpoint: '/api/moderator/model-photo/',
   photoDetailEndpoint: '/api/moderator/model-photo/',
-  getLobbiesEndpoint: 'http://localhost:8000/api/chat/',
+  getLobbiesEndpoint: 'http://singelsajten.se:8000/api/chat/',
   refreshEndpoint: '/jwt/refresh-token',
   logoutEndpoint: '/jwt/logout',
 
-  createChatEndpoint: 'http://localhost:8000/api/chat/',
-  chatDetailEndpoint: 'http://localhost:8000/api/chat/detail/',
-  LogsEndpoint: 'http://localhost:8000/api/chat/logs/',
+  createChatEndpoint: 'http://singelsajten.se:8000/api/chat/',
+  chatDetailEndpoint: 'http://singelsajten.se:8000/api/chat/detail/',
+  LogsEndpoint: 'http://singelsajten.se:8000/api/chat/logs/',
   massTriggerEndpoint: '/admin/mass-trigger/',
 
-  profileEndpoint: 'http://localhost:8000/auth/users/me/',
+  profileEndpoint: 'http://singelsajten.se:8000/auth/users/me/',
   // This will be prefixed in authorization header with token
   // e.g. Authorization: Bearer <token>
   tokenType: 'Token',
