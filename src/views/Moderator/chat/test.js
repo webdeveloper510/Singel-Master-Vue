e0bec6c06e3bba6ad3232414a7254578a2e3caf2
Satpal <deepsatpal>
2023-03-06 14:54:55 +0530

    changes

--- src/views/Moderator/chat/test.js
+++ src/views/Moderator/chat/test.js
@@ -1,23 +1,23 @@
 export default {
   // Endpoints
-  socketHost: '127.0.0.1',
-  loginEndpoint: 'http://127.0.0.1:8000/auth/token/login/',
-  registerEndpoint: 'http://127.0.0.1:8000/auth/users/',
-  createModelEndpoint: 'http://127.0.0.1:8000/api/moderator/model/',
-  createModeratorEndpoint: 'http://127.0.0.1:8000/api/admin/moderator/',
-  getCustomerEndpoint: 'http://127.0.0.1:8000/api/moderator/customers/',
-  uploadProfilePhotoEndpoint: 'http://127.0.0.1:8000/api/moderator/model-photo/',
-  photoDetailEndpoint: 'http://127.0.0.1:8000/api/moderator/model-photo/',
-  getLobbiesEndpoint: 'http://127.0.0.1:8000/api/chat/',
+  socketHost: 'singelsajten.se',
+  loginEndpoint: 'http://singelsajten.se:8000/auth/token/login/',
+  registerEndpoint: 'http://singelsajten.se:8000/auth/users/',
+  createModelEndpoint: 'http://singelsajten.se:8000/api/moderator/model/',
+  createModeratorEndpoint: 'http://singelsajten.se:8000/api/admin/moderator/',
+  getCustomerEndpoint: 'http://singelsajten.se:8000/api/moderator/customers/',
+  uploadProfilePhotoEndpoint: 'http://singelsajten.se:8000/api/moderator/model-photo/',
+  photoDetailEndpoint: 'http://singelsajten.se:8000/api/moderator/model-photo/',
+  getLobbiesEndpoint: 'http://singelsajten.se:8000/api/chat/',
   refreshEndpoint: '/jwt/refresh-token',
   logoutEndpoint: '/jwt/logout',
 
-  createChatEndpoint: 'http://127.0.0.1:8000/api/chat/',
-  chatDetailEndpoint: 'http://127.0.0.1:8000/api/chat/detail/',
-  LogsEndpoint: 'http://127.0.0.1:8000/api/chat/logs/',
+  createChatEndpoint: 'http://singelsajten.se:8000/api/chat/',
+  chatDetailEndpoint: 'http://singelsajten.se:8000/api/chat/detail/',
+  LogsEndpoint: 'http://singelsajten.se:8000/api/chat/logs/',
   massTriggerEndpoint: '/admin/mass-trigger/',
 
-  profileEndpoint: 'http://127.0.0.1:8000/auth/users/me/',
+  profileEndpoint: 'http://singelsajten.se:8000/auth/users/me/',
   // This will be prefixed in authorization header with token
   // e.g. Authorization: Bearer <token>
   tokenType: 'Token',
